a24567228c24ae3744543fc86b17bdc4bb51c2cd
RLant <rebeccalant@gmail.com>
2021-04-02 09:54:07 -0400

    notes on blur and focus

--- scripts.js
+++ scripts.js
@@ -1,3 +1,28 @@
 $(function(){
+  let inputFields = $("input:text, input:password, textarea");
+
+  inputFields.focus(function(){
+    $(this).css("box-shadow", "0 0 4px #2d41d6");
+  })
+
+
+  inputFields.blur(function(){
+    $(this).css("box-shadow", "none");
+  })
+
+  // Mini Challenge! Use theblurr event to check if the name has at least 3 characters. If it does not have at least 3 characters, you let the user know by making the focus red. Make a green box shadow if everything is correct. 
+
+
+// this is basic form validation
+  $("#name").blur(function(){
+    let text = $(this).val();
+    if(text.length < 3){
+      $(this).css("box-shadow", "0 0 4px red");
+    } else {
+      $(this).css("box-shadow", "0 0 4px green");
+    }
+
+  })
+
 
 });
